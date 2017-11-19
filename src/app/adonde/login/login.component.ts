6ba6d6adecb43a4651232013b343987d1d473d50
Zachary Freeman <zfreeman@smu.edu>
2017-11-19 17:06:15 -0600

    responsive forms

--- src/app/adonde/login/login.component.ts
+++ src/app/adonde/login/login.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterContentInit } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { User } from '../../domain';
+import { FormControl, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl } from '@angular/forms/src/model';
 
 @Component({
   selector: 'login',
@@ -9,10 +11,25 @@ import { User } from '../../domain';
 })
 export class LoginComponent {
   public user = new User();
+  changeData: FormGroup;
+  inputs: AbstractControl[] = [null, null];
 
-  private remember() {
+
+  constructor() {
+    this.changeData = new FormGroup({
+      emailInput: new FormControl(null, [Validators.required, Validators.email]),
+      passwordInput: new FormControl(null, [Validators.required, Validators.minLength(8)])
+    });
+
+    this.changeData.valueChanges.subscribe(val => this.update(val));
+    this.changeData.statusChanges.subscribe(val => this.update(val));
+  }
+
+  public remember() {
 
   }
 
-
+  public update(user: User) {
+    this.user = user;
+  }
 }
