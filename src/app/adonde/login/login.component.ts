64eb483551762d78762105850351c49ac69d2602
nnoakes <nnoakes@smu.edu>
2017-11-19 16:05:46 -0600

    Add files via upload

--- src/app/adonde/login/login.component.ts
+++ src/app/adonde/login/login.component.ts
@@ -11,6 +11,12 @@ export class LoginComponent {
   public user = new User();
 
   constructor(private http: HttpClient){}
+
+  ngOnInit(){
+    if(localStorage.getItem('email')){
+      
+    }
+  }
   
   private login(){
       this.http.post('http://ec2-18-216-113-131.us-east-2.compute.amazonaws.com/login',
@@ -30,7 +36,8 @@ export class LoginComponent {
   }
 
   private remember() {
-
+      localStorage.setItem('email', this.user.email);
+      localStorage.setItem('password', this.user.password);
   }
 
 
