57349d70504daabc5086fd8b19d48948691b7be9
Zachary Freeman <zfreeman@smu.edu>
2017-11-19 18:56:18 -0600

    updated login and remember functions

--- src/app/adonde/login/login.component.ts
+++ src/app/adonde/login/login.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input, AfterContentInit } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit, Input } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { User } from '../../domain';
 import { FormControl, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
 import { AbstractControl } from '@angular/forms/src/model';
@@ -9,25 +9,57 @@ import { AbstractControl } from '@angular/forms/src/model';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent {
   public user = new User();
   changeData: FormGroup;
   inputs: AbstractControl[] = [null, null];
+  wantRemember: boolean;
 
-
-  constructor() {
+  constructor(private http: HttpClient) {
     this.changeData = new FormGroup({
-      emailInput: new FormControl(null, [Validators.required, Validators.email]),
-      passwordInput: new FormControl(null, [Validators.required, Validators.minLength(8)])
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)])
     });
-
     this.changeData.valueChanges.subscribe(val => this.update(val));
-    this.changeData.statusChanges.subscribe(val => this.update(val));
+
+    this.wantRemember = false;
   }
 
-  public remember() {
-
+  ngOnInit() {
+    if (localStorage.getItem('email')) {
+      this.changeData.get('email').setValue(localStorage.getItem('email'));
+      // this.changeData.get('password').setValue(localStorage.getItem('password'));
+    }
   }
+
+  private login() {
+    if (this.changeData.valid) {
+      this.wantRemember ? localStorage.setItem('email', this.user.email) : localStorage.removeItem('email');
+      // localStorage.setItem('password', this.user.password);
+    }
+
+    this.http.post('http://ec2-18-216-113-131.us-east-2.compute.amazonaws.com/login',
+      {
+        email: this.user.email.toString(),
+        password: this.user.password.toString()
+      }
+    ).subscribe(data => {
+      console.log(data);
+      if (data['session_id'] == 401) {
+        alert('Wrong email or password');
+      } else {
+        localStorage.setItem('session_id', data['session_id']);
+      }
+    },
+      err => console.error(err),
+      () => console.log('done loading'));
+  }
+
+  private remember() {
+    this.wantRemember = !this.wantRemember;
+  }
+
 
   public update(user: User) {
     this.user = user;
