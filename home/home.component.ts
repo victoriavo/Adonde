1f41b7b6bcd79ede70c19a8bc66ce7d9a9cd522f
Zachary Freeman <zfreeman@smu.edu>
2017-11-27 22:48:14 -0600

    Update home.component.ts

--- home/home.component.ts
+++ home/home.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, Input } from '@angular/core';
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
-})
+}) 
 export class HomeComponent {
   
 
